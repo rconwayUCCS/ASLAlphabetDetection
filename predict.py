03acf3d70eac0acd3db1b9571adc3ef206776a34
Circe Conway <rconway@uccs.edu>
2023-03-22 17:56:56 -0600

    Displaying prediction on the image

--- predict.py
+++ predict.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import tensorflow as tf
-#from tensorflow import 
 from tensorflow.keras import datasets, layers, models
 from track_with_mediapipe import detect_hand
 
@@ -27,17 +26,21 @@ cap = cv2.VideoCapture(0)
 while(True):
     ret, frame = cap.read()
     output = detect_hand(frame, 64)
-    frame = cv2.resize(frame,(500,400))
-    cv2.imshow("Input", frame)
+    text = "None"
     if output is not None:
         output = np.reshape(output, (1, 64, 64, 1))
         prediction = model.predict(np.array(output))
         prediction_max = np.argmax(prediction)
         
-        print(alphabet[prediction_max])
-    else:
-        print("No Hand Detected")
+        text = alphabet[prediction_max]
+        #print(alphabet[prediction_max])
+    #else:
+        #print("No Hand Detected")
 
+    frame = cv2.resize(frame,(500,400))
+    image = cv2.putText(frame, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                 (0, 0, 0), 2, cv2.LINE_AA, False)
+    cv2.imshow("Video", image)
     if cv2.waitKey(1) & 0xFF == ord(' '):#exiting
         break
 
